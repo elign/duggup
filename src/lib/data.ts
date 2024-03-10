641f7fc6dd560efbb0b8b75ccde7b6686c2165f8
elign <harshitshivam001@gmail.com>
2024-03-10 10:23:28 +0530

    added data to the components

---
+++ src/lib/data.ts
@@ -0,0 +1,26 @@
+export interface CardData {
+  imgUrl: string;
+  text: string;
+}
+
+export let data1: CardData[] = [
+  {
+    imgUrl: "/steve.jpg",
+    text: "No amount of technology can convert a bad story into a good story.",
+  },
+  {
+    imgUrl: "/sam.webp",
+    text: "Most people don't have original ideas. Here is how Sam Altman pushes himself to have unpopular ideas.",
+  },
+];
+
+export let data2: CardData[] = [
+  {
+    imgUrl: "/desk.jpg",
+    text: "Startup Lesson I am reflecting. Don't build for the average person.",
+  },
+  {
+    imgUrl: "/sap.jpg",
+    text: "Your biggest regrets at 80 will be acts of omission.",
+  },
+];
